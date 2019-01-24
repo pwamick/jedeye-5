37f93a40d7f0553ae25f907b0dfc40aaa4bda03a
rcampbell761 <rcampbell761@gmail.com>
2019-01-24 08:49:06 -0600

    Initial Commit

---
+++ jedeye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jedeye
+//
+//  Created by Rick Campbell on 1/24/19.
+//  Copyright © 2019 Datacom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ jedeyeTests/jedeyeTests.swift
@@ -0,0 +1,34 @@
+//
+//  jedeyeTests.swift
+//  jedeyeTests
+//
+//  Created by Rick Campbell on 1/24/19.
+//  Copyright © 2019 Datacom. All rights reserved.
+//
+
+import XCTest
+@testable import jedeye
+
+class jedeyeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
