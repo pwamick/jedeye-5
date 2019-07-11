da6cff0e84bff179fa41dfebc1e1bc6fcf80780c
rcampbell761 <rcampbell761@gmail.com>
2019-07-11 18:19:19 -0500

    Not showing as being committed yet.

---
+++ jedeye/Main Flow/TerrainWeightViewController.swift
@@ -0,0 +1,61 @@
+//
+//  TerrainWeightViewController.swift
+//  jedeye
+//
+//  Created by Rick Campbell on 5/18/19.
+//  Copyright © 2019 Datacom. All rights reserved.
+//
+
+import UIKit
+
+class TerrainWeightViewController: UIViewController {
+    
+    //var survey : Survey?
+    //var navController : NavController?
+    
+    var promptText : String?
+    @IBOutlet weak var prompt : UILabel?
+    
+    @IBOutlet weak var txTerrainWeight : UITextField?
+    
+    @IBAction func btnNextClicked(sender:UIButton) {
+        if Session.getSelection()["Type"] as! String != "Telehandler" {
+            performSegue(withIdentifier: "FiveHundredSegue", sender: sender)
+        } else {
+            performSegue(withIdentifier: "LiftWeightSegue", sender: sender)
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        self.title = "Weight Max"
+        self.prompt!.text = promptText
+        
+        if let w = Session.surveyData?.surveySelections["TerWeight"] {
+            self.txTerrainWeight?.text = (w as! String)
+        }
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        Session.addToEquipmentSelectionDict(id:"TerWeight", value: self.txTerrainWeight!.text!)
+        Session.setKVPair(key: "TerWeight", value: self.txTerrainWeight!.text!)
+        switch segue.identifier {
+        case "FiveHundredSegue":
+            let destVC = segue.destination as? FiveHundredLbViewController
+            let nextOrdPos = "50"
+            destVC!.promptText = Session.getFieldDataFrom(Session.questions!, ordinalPosition: nextOrdPos, colName: "description")
+        case "LiftWeightSegue" :
+            let destVC = segue.destination as? LiftWeightViewController
+            let nextOrdPos = "56"
+            destVC!.promptText = Session.getFieldDataFrom(Session.questions!, ordinalPosition: nextOrdPos, colName: "description")
+        default:
+            print("&&No Such Segue")
+        }
+        
+    }
+    
+
+}
